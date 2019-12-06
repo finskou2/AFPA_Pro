f1965afa5cce99c2f2d99630c67d68fdf5d00a32
WilliamL92 <lavit.william@gmail.com>
2019-12-06 14:35:08 +0100

    site pour retrouver son chien sur une idée originale de CLEM

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    public function index(){
+        return $this->render('home.html.twig');
+    }
+}
+
+?>
